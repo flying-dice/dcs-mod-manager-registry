d13341f51710aa960d75d526d6210e33650b7f26
🐸 <dice-a6db@outlook.com>
2024-01-05 18:50:07 +0000

    add moose example to registry

--- build.js
+++ build.js
@@ -2,10 +2,18 @@ const {glob} = require("glob");
 const yaml = require("js-yaml");
 const fs = require("fs");
 const fse = require("fs-extra");
+const hash = require("object-hash");
 
 const mods = glob.sync("./registry/**/*.yml");
 
-const modData = mods.map(mod => yaml.load(fs.readFileSync(mod)))
+const modData = mods.map(modFile => {
+    const mod = yaml.load(fs.readFileSync(modFile))
+
+    return {
+        id: hash({url: mod.url}, {algorithm: "md5"}),
+        ...mod
+    }
+})
 
 fse.ensureDirSync("./dist")
 fse.writeJsonSync("./dist/registry.json", modData, {spaces: 2, newLine: "\n"})
