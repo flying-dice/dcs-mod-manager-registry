9132b8126af414a8daf66a299b480e8fc325a9bf
🐸 <dice-a6db@outlook.com>
2024-01-07 21:44:19 +0000

    feat: add octokit integration

--- functions/integrations/github.ts
+++ functions/integrations/github.ts
@@ -1,8 +1,22 @@
+import { Octokit } from "octokit"
+
+let octokit: Octokit;
+
 interface Env {
-  ENVIRONMENT: string;
+  GITHUB_TOKEN: string;
 }
 
+export const onRequest: PagesFunction<Env> = async (context): Promise<Response> => {
+  if (!octokit) {
+    octokit = new Octokit({
+      auth: context.env.GITHUB_TOKEN,
+    });
+  }
 
-export const onRequest: PagesFunction<Env> = (context): Response => {
-  return new Response(`Hello, world! ${context.env?.ENVIRONMENT}`)
+  const latest = await octokit.rest.repos.getLatestRelease({
+    owner: "flying-dice",
+    repo: "hello-world-mod",
+  });
+
+  return new Response(latest.data.tag_name)
 }
